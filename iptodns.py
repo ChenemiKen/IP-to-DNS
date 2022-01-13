fb8bf7ffabc70328ef9fb68bf43ce7558da6c5fb
KenDerick <chenemiken15@gmail.com>
2022-01-13 06:56:14 +0100

    initial commit

---
+++ iptodns.py
@@ -0,0 +1,6 @@
+import socket
+# domainName = 'www.google.com'
+# print(socket.gethostbyname(domainName))
+# print(socket.gethostname())
+# print(socket.gethostbyaddr('216.58.223.196'))
+# print(socket.gethostbyname(socket.gethostname()))
